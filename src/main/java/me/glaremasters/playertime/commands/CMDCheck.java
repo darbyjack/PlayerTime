1c323fc0901792b48a2f2dd7883ba692c79da9eb
darbyjack <admin@glaremasters.me>
2018-04-28 18:23:26 -0500

    Finish up the command

--- src/main/java/me/glaremasters/playertime/commands/CMDCheck.java
+++ src/main/java/me/glaremasters/playertime/commands/CMDCheck.java
@@ -3,6 +3,8 @@ package me.glaremasters.playertime.commands;
 import static me.glaremasters.playertime.utils.ColorUtil.color;
 import java.util.concurrent.TimeUnit;
 import me.glaremasters.playertime.PlayerTime;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,6 +18,7 @@ import org.bukkit.entity.Player;
 public class CMDCheck implements CommandExecutor {
 
     FileConfiguration config = PlayerTime.getI().getConfig();
+    FileConfiguration ptime = PlayerTime.getI().playTimeConfig;
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -34,7 +37,36 @@ public class CMDCheck implements CommandExecutor {
                                 TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(miliseconds)))
                 );
                 player.sendMessage(playTime);
+                return true;
             }
+            if (args.length == 1) {
+                if (!player.hasPermission("playertime.others")) {
+                    return false;
+                }
+                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[0]);
+                if (!offlinePlayer.hasPlayedBefore()) {
+                    player.sendMessage(color(config.getString("messages.never-played-before")));
+                    return true;
+                }
+                if (!ptime.contains(offlinePlayer.getUniqueId().toString())) {
+                    player.sendMessage(color(config.getString("messages.no-playtime-data")));
+                    return false;
+                }
+
+                int time = ptime.getInt(offlinePlayer.getUniqueId().toString());
+                int seconds = time / 20;
+                int miliseconds = seconds * 1000;
+
+                String playTime = color(String.format(config.getString("messages.check-others"), TimeUnit.MILLISECONDS.toMinutes(miliseconds),
+                        TimeUnit.MILLISECONDS.toSeconds(miliseconds) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(miliseconds))).replace("{name}", offlinePlayer.getName())
+                );
+                player.sendMessage(playTime);
+
+                return true;
+            }
+            player.sendMessage(color(config.getString("messages.incorrect-usage")));
+            return true;
         }
         return true;
     }
