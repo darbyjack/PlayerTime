81766c7755792d033122b47c817056234fd936d1
darbyjack <admin@glaremasters.me>
2018-04-28 17:24:50 -0500

    Save player time on leave

--- src/main/java/me/glaremasters/playertime/PlayerTime.java
+++ src/main/java/me/glaremasters/playertime/PlayerTime.java
@@ -1,5 +1,10 @@
 package me.glaremasters.playertime;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import me.glaremasters.playertime.events.Leave;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerTime extends JavaPlugin {
@@ -10,15 +15,31 @@ public final class PlayerTime extends JavaPlugin {
         return i;
     }
 
+    public File playTime = new File(this.getDataFolder(), "playtime.yml");
+    public YamlConfiguration playTimeConfig = YamlConfiguration.loadConfiguration(this.playTime);
+
     @Override
     public void onEnable() {
 
         i = this;
+        saveDefaultConfig();
+        saveTime();
+
+        getServer().getPluginManager().registerEvents(new Leave(), this);
 
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public void saveTime() {
+        try {
+            playTimeConfig.save(playTime);
+        } catch (IOException e) {
+            getLogger().log(Level.WARNING, "Could not save PlayTime Data!");
+            e.printStackTrace();
+        }
     }
 }
