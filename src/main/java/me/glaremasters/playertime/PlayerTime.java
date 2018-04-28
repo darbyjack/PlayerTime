10242064aa19c17a8d921d2002722eb4136819bd
darbyjack <admin@glaremasters.me>
2018-04-28 17:17:07 -0500

    Setup instance

--- src/main/java/me/glaremasters/playertime/PlayerTime.java
+++ src/main/java/me/glaremasters/playertime/PlayerTime.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerTime extends JavaPlugin {
 
+    private static PlayerTime i;
+
+    public static PlayerTime getI() {
+        return i;
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        i = this;
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
