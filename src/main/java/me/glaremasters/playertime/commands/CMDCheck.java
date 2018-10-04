ff7433c909d33b286d6a1ff07efc7082bcd82f4b
darbyjack <admin@glaremasters.me>
2018-10-04 00:02:51 -0500

    1.13

--- src/main/java/me/glaremasters/playertime/commands/CMDCheck.java
+++ src/main/java/me/glaremasters/playertime/commands/CMDCheck.java
@@ -62,9 +62,9 @@ public class CMDCheck implements CommandExecutor {
     public static int ticksToMillis(Player player) {
         int time;
         try {
-            time = player.getStatistic(Statistic.PLAY_ONE_TICK);
+            time = player.getStatistic(Statistic.PLAY_ONE_MINUTE);
         } catch (NoSuchFieldError e) {
-            time = player.getStatistic(Statistic.valueOf("PLAY_ONE_MINUTE"));
+            time = player.getStatistic(Statistic.valueOf("PLAY_ONE_TICK"));
         }
         int seconds = time / 20;
         int millis = seconds * 1000;
